d1a8868d05aa6d161f5718c5616ff346006cfa19
Shaunna <skwiens@gmail.com>
2018-01-18 13:51:28 -0800

    posts to google calendar

--- app/openhours/views.py
+++ app/openhours/views.py
@@ -5,6 +5,8 @@ from app import db
 from app.models import Openhour, Volunteer, Note
 from app.forms import OpenhourForm, NoteForm
 
+import googleapiclient.discovery
+import google.oauth2.credentials
 
 openhours_blueprint = Blueprint('openhours', __name__, template_folder='templates')
 
@@ -56,6 +58,54 @@ def new_openhour():
         return redirect(url_for('index'))
 
     return render_template('openhour_form.html', form=form)
+
+@openhours_blueprint.route('/<string:id>/post', methods=['GET', 'POST'])
+# @admin_logged_in
+def post_openhour(id):
+    ### POST TO GOOGLE calendar
+
+    ####### WORKS IF DONE WITHIN THE HOUR#################
+    credentials = google.oauth2.credentials.Credentials(**session['credentials'])
+    service = googleapiclient.discovery.build('calendar', 'v3', credentials=credentials)
+
+    #### INSUFFICIENT PERMISSION########
+    ##############################################
+    # credentials = get_credentials()
+    # http = credentials.authorize(httplib2.Http())
+    # service = discovery.build('calendar', 'v3', http=http)
+    ##############################################
+
+    openhour = Openhour.query.get(id)
+    date = '{:%Y-%m-%d}'.format(openhour.date)
+
+    # Post to calendar
+    for volunteer in openhour.volunteers:
+        event = {
+            'summary': 'OH: %s' % volunteer.name,
+            'start': {
+                'date': date
+            },
+            'end': {
+                'date': date
+            }
+        }
+        event = service.events().insert(calendarId='primary', body=event).execute()
+        print ('Event created: %s' % (event.get('htmlLink')))
+
+    for shopper in openhour.shoppers:
+        event = {
+            'summary': 'SHOP: %s' % shopper.name,
+            'start': {
+                'date': date
+            },
+            'end': {
+                'date': date
+            }
+        }
+        event = service.events().insert(calendarId='primary', body=event).execute()
+        print ('Event created: %s' % (event.get('htmlLink')))
+
+    return redirect(url_for('index'))
 
 @openhours_blueprint.route('/<string:id>/notes/new', methods=['GET', 'POST'])
 def new_notes(id):
