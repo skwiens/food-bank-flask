dd731cb98edb2607f49649f89d27452ecda2acec
Shaunna <skwiens@gmail.com>
2018-01-19 10:54:17 -0800

    set db to environmental var

--- rundev.sh
+++ rundev.sh
@@ -5,6 +5,8 @@ export SECRET_KEY='clavesecreto'
 export CLIENT_SECRET_FILE='client_secret.json'
 export CLIENT_ID='673297702802-meoqb93la7chfs2frqpbqrdqa0c0ts0k.apps.googleusercontent.com'
 export ADMIN_EMAIL='xana.wines.ada@gmail.com'
+export DATABASE_URL="sqlite:///$(pwd)/app.db"
 export CLIENT_SECRET='_kEtgRvZDc5oIqujt_Qle9FL'
+echo $DATABASE_URL
 
 python flask-app.py
