cbbb70244185aab6a352a71d7e0c9f6186dfcf06
Shaunna <skwiens@gmail.com>
2018-01-19 21:49:03 -0800

    another attempt at google oauth on heroku

--- app/oauth_helpers.py
+++ app/oauth_helpers.py
@@ -9,7 +9,7 @@ import google_auth_oauthlib.flow
 import google.oauth2.credentials
 import googleapiclient.discovery
 
-# CLIENT_SECRET_FILE = json.loads(os.environ['CLIENT_SECRETS_FILE'])
+CLIENT_SECRET_FILE = json.loads(os.environ['CLIENT_SECRETS_FILE'])
 # CLIENT_SECRET_FILE = os.environ['CLIENT_SECRETS_FILE']
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.compose', 'https://www.googleapis.com/auth/calendar']
@@ -17,20 +17,36 @@ APPLICATION_NAME='Bethany Food Bank'
 ADMIN_EMAIL = os.environ['ADMIN_EMAIL']
 
 def client_secrets():
+    # secret =  {
+    #     "web": {
+    #         "client_id": os.environ['CLIENT_ID'],
+    #         "client_secret": os.environ['CLIENT_SECRET'],
+    #         "redirect_uris": ['https://bethany-food-bank.herokuapp.com/oauth2callback', 'http://localhost:5000/oauth2callback'],
+    #         "auth_uri": 'https://accounts.google.com/o/oauth2/auth'
+    #         "token_uri": 'https://accounts.google.com/o/oauth2/token'
+    #     }
+    # }
+
     secret =  {
-        "web": {
-            "client_id": os.environ['CLIENT_ID'],
-            "client_secret": os.environ['CLIENT_SECRET'],
-            "redirect_uris": ['https://bethany-food-bank.herokuapp.com/oauth2callback', 'http://localhost:5000/oauth2callback'],
-            "auth_uri": 'https://accounts.google.com/o/oauth2/auth',
-            "token_uri": 'https://accounts.google.com/o/oauth2/token'
+        'web': {
+            'client_id': '673297702802-meoqb93la7chfs2frqpbqrdqa0c0ts0k.apps.googleusercontent.com',
+            'project_id': 'bethany-food-bank',
+            'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+            'token_uri': 'https://accounts.google.com/o/oauth2/token',
+            'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
+            'client_secret': '_kEtgRvZDc5oIqujt_Qle9FL',
+            'redirect_uris': ['https://bethany-food-bank.herokuapp.com/oauth2callback', 'http://localhost:5000/oauth2callback'],
+            'javascript_origins': ['https://bethany-food-bank-heroku.herokuapp.com', 'http://localhost:5000']
         }
     }
 
     return secret
 
+
+
+
 def credentials_to_dict(credentials):
-  return {'token': credentials.token,
+    return {'token': credentials.token,
           'refresh_token': credentials.refresh_token,
           'token_uri': credentials.token_uri,
           'client_id': credentials.client_id,
@@ -40,7 +56,6 @@ def credentials_to_dict(credentials):
 @app.route('/authorize')
 def authorize():
     CLIENT_SECRET_FILE = client_secrets()
-    print(CLIENT_SECRET_FILE)
     flow=google_auth_oauthlib.flow.Flow.from_client_config(client_config=CLIENT_SECRET_FILE, scopes=SCOPES)
 
     flow.redirect_uri = url_for('oauth2callback', _external=True)
