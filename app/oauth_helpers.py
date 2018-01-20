ab670d87c488380f9526d156d3cdd74b8e04d0fe
Shaunna <skwiens@gmail.com>
2018-01-19 22:03:36 -0800

    another attempt

--- app/oauth_helpers.py
+++ app/oauth_helpers.py
@@ -29,12 +29,12 @@ def client_secrets():
 
     secret =  {
         'web': {
-            'client_id': '673297702802-meoqb93la7chfs2frqpbqrdqa0c0ts0k.apps.googleusercontent.com',
+            'client_id': os.environ['CLIENT_ID'],
             'project_id': 'bethany-food-bank',
             'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
             'token_uri': 'https://accounts.google.com/o/oauth2/token',
             'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
-            'client_secret': '_kEtgRvZDc5oIqujt_Qle9FL',
+            'client_secret': os.environ['CLIENT_SECRET'],
             'redirect_uris': ['https://bethany-food-bank.herokuapp.com/oauth2callback', 'http://localhost:5000/oauth2callback'],
             'javascript_origins': ['https://bethany-food-bank-heroku.herokuapp.com', 'http://localhost:5000']
         }
--- rundev.sh
+++ rundev.sh
@@ -2,11 +2,11 @@ export DEBUG=True
 export OAUTHLIB_INSECURE_TRANSPORT=1
 export SECRET_KEY='clavesecreto'
 # export CLIENT_SECRET_FILE='client_secret.json'
-export CLIENT_SECRETS_FILE='{"web":{"client_id":"673297702802-meoqb93la7chfs2frqpbqrdqa0c0ts0k.apps.googleusercontent.com","project_id":"bethany-food-bank","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"_kEtgRvZDc5oIqujt_Qle9FL","redirect_uris":["https://bethany-food-bank.herokuapp.com/oauth2callback","http://localhost:5000/oauth2callback"],"javascript_origins":["https://bethany-food-bank-heroku.herokuapp.com","http://localhost:5000"]}}'
-export CLIENT_ID='673297702802-meoqb93la7chfs2frqpbqrdqa0c0ts0k.apps.googleusercontent.com'
+export CLIENT_SECRETS_FILE='{"web":{"client_id":"673297702802-r4d8gnq8hamha90la46ghjeodakadd83.apps.googleusercontent.com","project_id":"bethany-food-bank","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"yNkGWUO9oIxjxqbxYlClvJKs","redirect_uris":["https://bethany-food-bank.herokuapp.com/oauth2callback","http://localhost:5000/oauth2callback","http://localhost:8080/oauth2callback","http://localhost:8080/"],"javascript_origins":["https://bethany-food-bank.herokuapp.com","http://localhost:5000"]}}'
+export CLIENT_ID='673297702802-r4d8gnq8hamha90la46ghjeodakadd83.apps.googleusercontent.com'
 export ADMIN_EMAIL='xana.wines.ada@gmail.com'
 export DATABASE_URL="sqlite:///$(pwd)/app.db"
-export CLIENT_SECRET='_kEtgRvZDc5oIqujt_Qle9FL'
+export CLIENT_SECRET='yNkGWUO9oIxjxqbxYlClvJKs'
 echo $DATABASE_URL
 
 python flask-app.py
